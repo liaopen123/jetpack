ea6537c58e9ec37898153bbc3df1f2e987ca9378
廖鹏辉 <467969@qq.com>
2020-03-23 11:13:12 +0800

    Initial commit

---
+++ app/src/main/java/cn/almostlover/jetpack/MainActivity.kt
@@ -0,0 +1,24 @@
+package cn.almostlover.jetpack
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
+        textView.text = "${viewModel.count}"
+        button.setOnClickListener {
+            viewModel.count++
+            textView.text = "${viewModel.count}"
+        }
+        button2.setOnClickListener {
+            viewModel.count += 2
+            textView.text = "${viewModel.count}"
+        }
+    }
+}
---
+++ app/src/main/java/cn/almostlover/jetpack/MyViewModel.kt
@@ -0,0 +1,8 @@
+package cn.almostlover.jetpack
+
+import androidx.lifecycle.ViewModel
+
+class MyViewModel :ViewModel(){
+    var count :Int=0
+
+}
